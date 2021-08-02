24cacef4c82572df271fb57095ed1441aa101fd7
imaginalis <rafbel001@utp.edu.pl>
2021-08-02 07:20:18 +0200

    Add captcha domain classes declarations

---
+++ src/main/kotlin/net/mestwin/playground/captcha/domain/CaptchaDeclarations.kt
@@ -0,0 +1,16 @@
+package net.mestwin.playground.captcha.domain
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class CaptchaRequest(val captchaAnswer: String)
+
+class CaptchaResponse(
+    val success: Boolean? = null,
+    val score: Float? = null,
+    val action: String? = null,
+    private val hostname: String? = null,
+    @JsonProperty("challenge_ts")
+    private val challengeTimestamp: String? = null,
+    @JsonProperty("error-codes")
+    private val errorCodes: List<String>? = null
+)
